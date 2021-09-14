defaa2ee45dbd9aa9e97c43ef9a295c1c77ad533
alex-z <blackslayer4@gmail.com>
2021-09-14 14:02:11 +0300

    Fetch Unified Search results with category order included.
    Signed-off-by: alex-z <blackslayer4@gmail.com>

--- src/gui/tray/UnifiedSearchResultsListModel.cpp
+++ src/gui/tray/UnifiedSearchResultsListModel.cpp
@@ -18,6 +18,8 @@
 #include "accountstate.h"
 #include "networkjobs.h"
 
+#include <algorithm>
+
 namespace OCC {
 
 UnifiedSearchResultsListModel::UnifiedSearchResultsListModel(AccountState *accountState, QObject *parent)
@@ -78,13 +80,14 @@ void UnifiedSearchResultsListModel::slotSearchTermEditingFinished()
         QObject::connect(job, &JsonApiJob::jsonReceived, [&, this](const QJsonDocument &json) {
             const auto providerList = json.object().value("ocs").toObject().value("data").toVariant().toList();
 
-            _providers.reserve(providerList.size());
-
             for (const auto& provider : providerList) {
                 const auto providerMap = provider.toMap();
-                const auto providerId = providerMap["id"].toString();
-                if (!providerId.isEmpty()) {
-                    _providers.push_back(providerId);
+                Provider provider;
+                provider._name = providerMap["name"].toString();
+                if (!provider._name.isEmpty()) {
+                    provider._id = providerMap["id"].toString();
+                    provider._order = providerMap["order"].toInt();
+                    _providers.insert(provider._name, provider);
                 }
             }
 
@@ -100,24 +103,41 @@ void UnifiedSearchResultsListModel::slotSearchTermEditingFinished()
 
 void UnifiedSearchResultsListModel::startSearch()
 {
+    _resultsByCategory.clear();
+
     for (const auto& provider : _providers) {
         startSearchForProvider(provider);
     }
 }
 
-void UnifiedSearchResultsListModel::startSearchForProvider(const QString &providerId)
+void UnifiedSearchResultsListModel::startSearchForProvider(const Provider &provider)
 {
-    auto *job = new JsonApiJob(_accountState->account(), QLatin1String("ocs/v2.php/search/providers/%1/search").arg(providerId));
+    auto *job = new JsonApiJob(_accountState->account(), QLatin1String("ocs/v2.php/search/providers/%1/search").arg(provider._id));
     QUrlQuery params;
     params.addQueryItem(QStringLiteral("term"), searchTerm());
     job->addQueryParams(params);
-    QObject::connect(job, &JsonApiJob::jsonReceived, [&](const QJsonDocument &json) {
-        auto data = json.object().value("ocs").toObject().value("data").toObject();
+    QObject::connect(job, &JsonApiJob::jsonReceived, [&, provider](const QJsonDocument &json) {
+        const auto data = json.object().value("ocs").toObject().value("data").toObject();
         if (!data.isEmpty()) {
             const auto name = data.value("name").toString();
+            const auto providerForResults = _providers.find(name);
             const auto isPaginated = data.value("isPaginated").toBool();
             const auto cursor = data.value("cursor").toInt();
-            const auto entries = data.value("entries").toVariant();
+            const auto entries = data.value("entries").toVariant().toList();
+
+            if (providerForResults != _providers.end() && !entries.isEmpty()) {
+                UnifiedSearchResultCategory category;
+                category._order = (*providerForResults)._order;
+                category._name = (*providerForResults)._name;
+
+                for (const auto &entry : entries) {
+                    UnifiedSearchResult result;
+                    result._title = entry.toMap()["title"].toString();
+                    category._results.push_back(result);
+                }
+
+                _resultsByCategory.push_back(category);
+            }
         }
     });
     job->start();
